47f3ac8c89b99f117e5e27fe3d1ea19e8265cba7
Eugene Gatsak <greenglaz@gmail.com>
2023-11-16 22:25:33 +0100

    fix: update api paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,5 @@
 const API_PATHS = {
-  product:
-    "https://nodejs-aws-shop-react-egatsak.execute-api.eu-north-1.amazonaws.com/dev",
+  product: "https://2vrehcwum5.execute-api.eu-north-1.amazonaws.com",
   order:
     "https://nodejs-aws-shop-react-egatsak.execute-api.eu-north-1.amazonaws.com/dev",
   import:
