76d93e930bd8a90d256d073ee885dcbbea08d2ab
Eugene Gatsak <greenglaz@gmail.com>
2023-12-16 18:17:45 +0100

    fix: update apipaths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://2vrehcwum5.execute-api.eu-north-1.amazonaws.com",
   order:
     "https://nodejs-aws-shop-react-egatsak.execute-api.eu-north-1.amazonaws.com/dev",
-  import: "https://6iu6ypqf11.execute-api.eu-north-1.amazonaws.com/prod",
+  import: "https://7ba7cl4i25.execute-api.eu-north-1.amazonaws.com/prod",
   bff: "https://nodejs-aws-shop-react-egatsak.execute-api.eu-north-1.amazonaws.com/dev",
   cart: "https://nodejs-aws-shop-react-egatsak.execute-api.eu-north-1.amazonaws.com/dev",
 };
