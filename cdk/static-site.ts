66ca23376395eac1a1cf300ab72f7aa996f6aba3
Eugene Gatsak <greenglaz@gmail.com>
2024-06-06 00:49:46 +0200

    feat: implement cloudfront deploy

--- cdk/static-site.ts
+++ cdk/static-site.ts
@@ -6,14 +6,15 @@ import { aws_s3 as s3 } from "aws-cdk-lib"; // stable module
 import { aws_s3_deployment as s3deploy } from "aws-cdk-lib";
 import { aws_iam as iam } from "aws-cdk-lib";
 import { aws_cloudfront as cloudfront } from "aws-cdk-lib";
+import { CfnOutput, RemovalPolicy } from "aws-cdk-lib";
 
 export class StaticSite extends Construct {
   constructor(parent: Stack, name: string) {
     super(parent, name);
-    /* 
-    const cfnExecRole = new iam.Role(this, "CfnExecRole", {
+
+    /* const cfnExecRole = new iam.Role(this, "CfnExecRole", {
       assumedBy: new iam.ArnPrincipal(
-        "arn:aws:sts::263622645179:assumed-role/AWSReservedSSO_PowerUserAccess_7a980f367bc73c6d/egatsak-iam-poweruser_not_admin"
+        "arn:aws:sso:::permissionSet/ssoins-6987134914726260/ps-4b533229d7158adb"
       ),
     });
 
@@ -36,19 +37,33 @@ export class StaticSite extends Construct {
     // Attach the managed policy to the role
     cfnExecRole.addManagedPolicy(inlinePolicy); */
 
-    const cloudFrontOAI = new cloudfront.OriginAccessIdentity(
+    /*     const cloudFrontOAI = new cloudfront.OriginAccessIdentity(
       this,
       "MyShopCdk-OAI"
+    ); */
+
+    const originAccessControl = new cloudfront.CfnOriginAccessControl(
+      this,
+      "MyOriginAccessControl",
+      {
+        originAccessControlConfig: {
+          name: "nodejs-aws-shop-react-egatsak-origin-access-control",
+          originAccessControlOriginType: "s3",
+          signingProtocol: "sigv4",
+          signingBehavior: "always",
+        },
+      }
     );
 
     const siteBucket = new s3.Bucket(this, "MyShopFrontendStaticBucket", {
       bucketName: "nodejs-aws-shop-react-egatsak-cdk",
       websiteIndexDocument: "index.html",
       publicReadAccess: false,
+      encryption: s3.BucketEncryption.S3_MANAGED,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
     });
 
-    siteBucket.addToResourcePolicy(
+    /*  siteBucket.addToResourcePolicy(
       new iam.PolicyStatement({
         actions: ["S3:GetObject"],
         resources: [siteBucket.arnForObjects("*")],
@@ -58,9 +73,9 @@ export class StaticSite extends Construct {
           ),
         ],
       })
-    );
+    ); */
 
-    const distribution = new cloudfront.CloudFrontWebDistribution(
+    /*     const distribution = new cloudfront.CloudFrontWebDistribution(
       this,
       "MyShopCdk-Distribution",
       {
@@ -78,6 +93,68 @@ export class StaticSite extends Construct {
           },
         ],
       }
+    ); */
+
+    const distribution = new cloudfront.CloudFrontWebDistribution(
+      this,
+      "MyCloudFrontDistribution",
+      {
+        defaultRootObject: "index.html",
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.HTTPS_ONLY,
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: siteBucket,
+            },
+            behaviors: [
+              {
+                isDefaultBehavior: true,
+              },
+            ],
+          },
+        ],
+      }
+    );
+
+    const cfnDistribution = distribution.node
+      .defaultChild as cloudfront.CfnDistribution;
+    cfnDistribution.addPropertyOverride(
+      "DistributionConfig.Origins.0.S3OriginConfig.OriginAccessIdentity",
+      ""
+    );
+    cfnDistribution.addPropertyOverride(
+      "DistributionConfig.Origins.0.OriginAccessControlId",
+      originAccessControl.getAtt("Id")
+    );
+
+    // workaround to use Cache Policy instead of legacy Cache Behavior
+    cfnDistribution.addPropertyDeletionOverride(
+      "DistributionConfig.DefaultCacheBehavior.AllowedMethods"
+    );
+    cfnDistribution.addPropertyDeletionOverride(
+      "DistributionConfig.DefaultCacheBehavior.CachedMethods"
+    );
+    cfnDistribution.addPropertyDeletionOverride(
+      "DistributionConfig.DefaultCacheBehavior.ForwardedValues"
+    );
+    cfnDistribution.addPropertyOverride(
+      "DistributionConfig.DefaultCacheBehavior.CachePolicyId",
+      cloudfront.CachePolicy.CACHING_OPTIMIZED.cachePolicyId
+    );
+
+    const distributionArn = `arn:aws:cloudfront::${distribution.stack.account}:distribution/${distribution.distributionId}`;
+
+    siteBucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        actions: ["s3:GetObject"],
+        resources: [siteBucket.arnForObjects("*")],
+        principals: [new iam.ServicePrincipal("cloudfront.amazonaws.com")],
+        conditions: {
+          StringEquals: {
+            "AWS:SourceArn": distributionArn,
+          },
+        },
+      })
     );
 
     new s3deploy.BucketDeployment(this, "MyShopCdk-Bucket-Deployment", {
@@ -85,6 +162,17 @@ export class StaticSite extends Construct {
       destinationBucket: siteBucket,
       distribution,
       distributionPaths: ["/*"],
+    });
+
+    distribution.applyRemovalPolicy(RemovalPolicy.DESTROY);
+    originAccessControl.applyRemovalPolicy(RemovalPolicy.DESTROY);
+
+    new CfnOutput(this, "S3BucketUrl", {
+      value: siteBucket.bucketWebsiteUrl,
+    });
+
+    new CfnOutput(this, "CloudFrontUrl", {
+      value: distribution.distributionDomainName,
     });
   }
 }
