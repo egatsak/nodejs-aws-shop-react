32bd1e0f77123134c9ccca6ebfe889f5502bf7f5
Eugene Gatsak <greenglaz@gmail.com>
2024-07-28 18:55:15 +0200

    feat: update api paths for cart api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://km96rjp673.execute-api.us-east-1.amazonaws.com/dev",
-  order: "https://w9ws87wsrf.execute-api.us-east-1.amazonaws.com/dev/api",
+  order: "https://ewqlete33h.execute-api.us-east-1.amazonaws.com/api",
   import: "https://i3jtq6hsag.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://nodejs-aws-shop-react-egatsak.execute-api.eu-north-1.amazonaws.com/dev",
-  cart: "https://w9ws87wsrf.execute-api.us-east-1.amazonaws.com/dev/api",
+  cart: "https://ewqlete33h.execute-api.us-east-1.amazonaws.com/api",
 };
 
 export default API_PATHS;
