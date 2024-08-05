2cfcfc2d2587db97642128768e980e7c0b5cfd55
Eugene Gatsak <greenglaz@gmail.com>
2024-08-05 14:07:03 +0200

    fix: update apipaths to point to bff service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://km96rjp673.execute-api.us-east-1.amazonaws.com/dev",
   order: "https://ewqlete33h.execute-api.us-east-1.amazonaws.com/api",
   import: "https://i3jtq6hsag.execute-api.us-east-1.amazonaws.com/dev",
-  bff: "https://nodejs-aws-shop-react-egatsak.execute-api.eu-north-1.amazonaws.com/dev",
+  bff: "https://y7yrqfp8r9.execute-api.us-east-1.amazonaws.com/bff",
   cart: "https://ewqlete33h.execute-api.us-east-1.amazonaws.com/api",
 };
 
